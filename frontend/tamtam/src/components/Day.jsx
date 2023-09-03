c533a2951e1c6dd06b7e70b511605eaf9f181652
wnsdk <jn307742@gmail.coom>
2023-09-04 04:15:37 +0900

    feat: 병합 기능 구현 직전 상태

--- frontend/tamtam/src/components/Day.jsx
+++ frontend/tamtam/src/components/Day.jsx
@@ -1,59 +1,157 @@
 // 시간표 하루 컬럼
 
-import { React, useState, useRef } from "react";
+import { React, useState, useRef, useEffect } from "react";
 import style from "./Day.module.css";
+import { useRecoilState } from "recoil";
+// import {
+//   focusedDayAtom,
+//   clickedIndexStartAtom,
+//   clickedIndexEndAtom,
+//   focusedIndexAtom,
+// } from "util/store";
 
-export default function Day() {
+export default function Day({ date, day, isFocused, ref }) {
   /** 데이터 */
   const [act, setAct] = useState(Array.from({ length: 144 }, () => ""));
-  const [ClickedIndex, setClickedIndex] = useState(); // 클릭된 인덱스 번호 (className 변경하기 위함)
+  const [clickedIndexStart, setClickedIndexStart] = useState(-1); // 클릭된 인덱스 시작 번호 (className 변경하기 위함)
+  const [clickedIndexEnd, setClickedIndexEnd] = useState(-1); // 클릭된 인덱스 끝 번호
+  const [focusedIndex, setFocusedIndex] = useState(); // 포커싱된 인덱스
+  // const [focusedDay, setFocusedDay] = useRecoilState(focusedDayAtom);
+  // const [clickedIndexStart, setClickedIndexStart] = useRecoilState(
+  //   clickedIndexStartAtom
+  // ); // 클릭된 인덱스 시작 번호
+  // const [clickedIndexEnd, setClickedIndexEnd] =
+  //   useRecoilState(clickedIndexEndAtom); // 클릭된 인덱스 끝 번호
+  // const [focusedIndex, setFocusedIndex] = useRecoilState(focusedIndexAtom); // 포커싱된 인덱스
 
-  const dragItemIndex = useRef(null);
-  const inputRefs = useRef([]);
+  const inputRefs1 = useRef([]);
+  const inputRefs2 = useRef([]);
 
   /** 함수 */
+  // 더블 클릭 시
   const onDoubleClick = (idx) => {
-    inputRefs.current[idx]?.focus();
-  };
-  const onMouseDown = (e, idx) => {
-    // 마우스 1번 클릭 시 focus 비활성화
-    e.preventDefault();
-    document.activeElement.blur();
-    setClickedIndex(idx);
+    setFocusedIndex(idx);
   };
 
-  const onDragStart = () => {
-    console.log("드래그 시작");
+  // 마우스 1번 클릭 시
+  const onMouseDown = (e, idx) => {
+    // focus 비활성화
+    // if (clickedIndexStart !== idx) {
+    //   e.preventDefault();
+    // }
+    // 기존에 focus 된 칸을 클릭한 게 아니라면
+    if (focusedIndex !== idx) {
+      setFocusedIndex(-1);
+    }
+
+    document.activeElement.blur();
+    setClickedIndexStart(idx);
+    setClickedIndexEnd(idx);
   };
-  const onDragEnd = (e) => {
-    // e.target.classList.remove("grabbing");
-    console.log("드래그 끝");
+
+  // 드래그 시작
+  const onDragStart = (idx) => {};
+
+  // 드래그 진입
+  const onDragEnter = (idx) => {
+    if (!isFocused) return;
+    setClickedIndexEnd(idx);
   };
+
+  // 드래그 끝
+  const onDrop = (e, idx) => {
+    e.preventDefault();
+    if (clickedIndexStart > clickedIndexEnd) {
+      let tmp = clickedIndexStart;
+      setClickedIndexStart(clickedIndexEnd);
+      setClickedIndexEnd(tmp);
+    }
+    merge();
+  };
+
+  // 병합
+  const merge = () => {
+    console.log("병합합니당");
+    // clickedIndexStart
+  };
+
+  /** useEffect */
+  useEffect(() => {
+    // 클릭된 인풋 태그의 인덱스가 바뀌면, 해당 인풋 태그를 포커싱하기
+    inputRefs1.current[clickedIndexStart]?.focus();
+  }, [clickedIndexStart]);
+
+  useEffect(() => {
+    // 포커싱된 인풋 태그의 인덱스가 바뀌면, 해당 인풋 태그를 포커싱하기
+    inputRefs2.current[focusedIndex]?.focus();
+  }, [focusedIndex]);
 
   return (
     <>
-      <div className={style.days}>
-        <div className={style.day}>3일</div>
-        <div className={style.week}>일요일</div>
+      <div className={style.dates}>
+        <div className={style.date}>{date}일</div>
+        <div className={style.day}>{day}</div>
         <div className={style.minutes}>
           {act.map((item, idx) => (
             <div
               key={idx}
-              className={ClickedIndex === idx ? style.clicked : ""}
-              // onDragStart={() => onDragStart}
-              // onDragEnd={() => onDragEnd}
-              // onClick={() => onDragStart}
+              className={
+                isFocused &&
+                Math.min(clickedIndexStart, clickedIndexEnd) <= idx &&
+                idx <= Math.max(clickedIndexStart, clickedIndexEnd)
+                  ? style.clicked
+                  : ""
+              }
+              onDragStart={() => onDragStart(idx)}
+              onDragEnter={() => {
+                onDragEnter(idx);
+              }}
+              onDragOver={(e) => {
+                e.preventDefault();
+              }}
+              onDrop={(e) => {
+                onDrop(e, idx);
+              }}
+              onMouseDown={(e) => {
+                onMouseDown(e, idx);
+              }}
+              onDoubleClick={() => onDoubleClick(idx)}
+              draggable={focusedIndex !== idx}
             >
-              <input
-                ref={(element) => {
-                  inputRefs.current[idx] = element;
-                }}
-                onDoubleClick={() => onDoubleClick(idx)}
-                onMouseDown={(e) => {
-                  onMouseDown(e, idx);
-                }}
-                // value={}
-              />
+              {!isFocused || focusedIndex !== idx ? (
+                <input
+                  className={style.notFocused}
+                  ref={(element) => {
+                    inputRefs1.current[idx] = element;
+                  }}
+                  // style={{ color: "red" }}
+                  value={act[idx]}
+                  onKeyPress={() => {
+                    setFocusedIndex(idx);
+                  }}
+                  readOnly
+                />
+              ) : (
+                <input
+                  ref={(element) => {
+                    inputRefs2.current[idx] = element;
+                  }}
+                  onChange={(e) => {
+                    let newAct = [...act];
+                    newAct[idx] = e.target.value;
+                    setAct(newAct);
+                  }}
+                  onKeyDown={(e) => {
+                    // TODO : 화살표 키보드 안먹힘 (스크롤만 내려감)
+                    if (e.keyCode === 13 || e.keyCode === 34) {
+                      setClickedIndexStart(idx + 1);
+                      setClickedIndexEnd(idx + 1);
+                      setFocusedIndex(idx + 1);
+                    }
+                  }}
+                  value={act[idx]}
+                />
+              )}
             </div>
           ))}
         </div>
--- frontend/tamtam/src/pages/Main.jsx
+++ frontend/tamtam/src/pages/Main.jsx
@@ -1,19 +1,60 @@
-import React from "react";
+import { React, useState } from "react";
 import style from "./Main.module.css";
 import TableHeader from "components/TableHeader";
 import Day from "components/Day";
 
+// const days = ["일", "월", "화", "수", "목", "금", "토"];
+const days = [
+  "일요일",
+  "월요일",
+  "화요일",
+  "수요일",
+  "목요일",
+  "금요일",
+  "토요일",
+];
+
 export default function Main() {
+  let today = new Date();
+  // let year = today.getFullYear; // 년
+  let month = today.getMonth() + 1; // 월
+  let date = today.getDate(); // 일
+  let day = today.getDay(); // 요일
+  let startDate = date - day; // 시작 기준일
+
+  const [focusedDay, setFocusedDay] = useState(0); // 포커싱된 요일
+
+  /** 메서드 */
+  // 저장 (TODO : 버튼을 직접 누르지 않고 몇 초마다 자동 저장하기)
+  const store = () => {
+    alert("아직 서버가 준비 안됐습니당^^");
+  };
+
+  // 병합
+  const merge = () => {};
+
   return (
     <>
+      <div className={style.menu}>
+        <button onClick={() => merge()}>병합</button>
+        <button onClick={() => store()}>저장</button>
+      </div>
       <div className={style.table}>
-        <TableHeader />
-        <Day />
-        <Day />
-        <Day />
-        <Day />
-        <Day />
-        <Day />
+        <TableHeader month={month} />
+        {Array.from({ length: 7 }).map((item, idx) => (
+          <div
+            key={idx}
+            onMouseDown={() => {
+              setFocusedDay(idx);
+            }}
+          >
+            <Day
+              date={startDate + idx}
+              day={days[idx]}
+              isFocused={focusedDay === idx}
+            />
+          </div>
+        ))}
       </div>
     </>
   );
