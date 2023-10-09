f66a9d28ec2099ef3ffbdf10714b060f452f5381
wnsdk <jn307742@gmail.com>
2023-10-09 13:38:15 +0900

    spring 프로젝트 생성

---
+++ backend/owners/src/main/java/com/secretaries/owners/OwnersApplication.java
@@ -0,0 +1,13 @@
+package com.secretaries.owners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnersApplication.class, args);
+	}
+
+}
---
+++ backend/owners/src/test/java/com/secretaries/owners/OwnersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secretaries.owners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
