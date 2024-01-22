11b37bf2cd78f426e6ebac2bf2a6895009261d6e
wnsdk <jn307742@gmail.com>
2024-01-22 21:28:11 +0900

    Add axios 및 react query 설정 추가

--- frontend/fillsa/src/main.jsx
+++ frontend/fillsa/src/main.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-
 import { BrowserRouter } from 'react-router-dom';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={true} />
         <BrowserRouter>
             <App />
         </BrowserRouter>
-    </React.StrictMode>
+    </QueryClientProvider>
 );
--- frontend/fillsa/src/pages/MainPage.jsx
+++ frontend/fillsa/src/pages/MainPage.jsx
@@ -1,3 +1,33 @@
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { customTestAxios } from '../apis/customTestAxios';
+
 export default function MainPage() {
-    return <>Main Page</>;
+    // const { isLoading, error, data, isFetching } = useQuery({
+    //     queryKey: ['getPost'],
+    //     queryFn: async () => {
+    //         const response = await customTestAxios.get('posts/1');
+    //         return response.data;
+    //     },
+    // });
+
+    // const { mutate, isLoading, isError, error, isSuccess, data } = useMutation({
+    //     mutationFn: async () => {
+    //         return await customTestAxios.delete('posts/1');
+    //     },
+    // });
+
+    const { mutate, isLoading, isError, error, isSuccess, data } = useMutation({
+        mutationFn: async () => {
+            return await fetch('https://koreanjson.com/posts/1', { method: 'DELETE' });
+        },
+    });
+
+    return (
+        <>
+            <div>Main Page</div>
+            <button onClick={() => mutate()}>글 삭제</button>
+            {isError && <div>삭제 실패 {error.message}</div>}
+            {isSuccess && console.log(data.Response)}
+        </>
+    );
 }
