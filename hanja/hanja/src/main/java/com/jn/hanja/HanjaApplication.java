74835a992343363ba8262cd262df0249d02d5d49
wnsdk <jn307742@gmail.com>
2024-05-06 13:09:50 +0900

    create spring project

---
+++ hanja/hanja/src/main/java/com/jn/hanja/HanjaApplication.java
@@ -0,0 +1,13 @@
+package com.jn.hanja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanjaApplication.class, args);
+	}
+
+}
