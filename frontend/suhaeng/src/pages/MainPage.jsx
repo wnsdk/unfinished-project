03a789dca98cfdb8e21dd50804921d7f1f33ce22
wnsdk <jn307742@gmail.com>
2024-06-02 18:03:53 +0900

    Add: 로그인 절반 정도 구현

--- frontend/suhaeng/src/pages/MainPage.jsx
+++ frontend/suhaeng/src/pages/MainPage.jsx
@@ -1,96 +1,109 @@
-import styles from "./MainPage.module.scss";
-import Component1 from "@/components/Component1";
-import { useQuery } from "@tanstack/react-query";
-import { customAxios } from "../apis/customAxios";
+import styles from './MainPage.module.scss';
+import Component1 from '@/components/Component1';
+import { useQuery } from '@tanstack/react-query';
+import { customAxios } from '../apis/customAxios';
 // import candlelight from '@/assets/다운로드-removebg-preview.png';
-import { useState } from "react";
-import { useLoginStore } from "@/store/memberStore";
+import { useState } from 'react';
+import { useLoginStore } from '@/store/memberStore';
 
 export default function MainPage() {
-  const {
-    isLoading,
-    error,
-    data: feeds,
-    isFetching,
-  } = useQuery({
-    queryKey: ["getFeeds"],
-    queryFn: async () => {
-      const response = await customAxios.get("feed");
-      return response.data;
-    },
-  });
+    const {
+        isLoading,
+        error,
+        data: feeds,
+        isFetching,
+    } = useQuery({
+        queryKey: ['getFeeds'],
+        queryFn: async () => {
+            const response = await customAxios.get('feed');
+            return response.data;
+        },
+    });
 
-  const [cnt, setCnt] = useState(3124);
+    // const { isLoading, error, data, isFetching } = useQuery({
+    //     queryKey: ['googleLogin'],
+    //     queryFn: async () => {
+    //         const response = await customAxios.get('feed');
+    //         return response.data;
+    //     },
+    // });
 
-  const loginUser = useLoginStore((state) => state);
+    const [cnt, setCnt] = useState(3124);
 
-  return (
-    <div className={styles.body}>
-      <div className={styles.gradient}>
-        <div className={styles.box2}>
-          <div className={styles.box2_1}>
-            {!isLoading &&
-              feeds.map((feed, i) => <Component1 key={i} feed={feed} />)}
-          </div>
+    const loginUser = useLoginStore((state) => state);
 
-          {loginUser.isLogin ? (
-            <div className={styles.box2_2}>
-              <div>
-                <input type="radio" id="all" name="permission" />
-                <label htmlFor="all">전체 공개</label>
-                <input type="radio" id="group" name="permission" />
-                <label htmlFor="group">그룹 공개</label>
-                <input type="radio" id="me" name="permission" />
-                <label htmlFor="me">비공개</label>
-              </div>
+    function login(thirdPartyId) {
+        window.location.href = `http://localhost:8080/oauth2/authorization/${thirdPartyId}`;
+    }
 
-              <div className={styles.box2_2_2}>
-                <img
-                  className={styles.myProfileImg}
-                  src={loginUser.profileUrl}
-                />
-                <div className={styles.textbox}>
-                  <textarea placeholder="2024년 5월 30일, 오늘의 수행을 도반들에게 나눠주세요."></textarea>
-                  <div className={styles.submit}>전송</div>
+    return (
+        <div className={styles.body}>
+            <div className={styles.gradient}>
+                <div className={styles.box2}>
+                    <div className={styles.box2_1}>
+                        {!isLoading && feeds.map((feed, i) => <Component1 key={i} feed={feed} />)}
+                    </div>
+
+                    {loginUser.isLogin ? (
+                        <div className={styles.box2_2}>
+                            <div>
+                                <input type="radio" id="all" name="permission" />
+                                <label htmlFor="all">전체 공개</label>
+                                <input type="radio" id="group" name="permission" />
+                                <label htmlFor="group">그룹 공개</label>
+                                <input type="radio" id="me" name="permission" />
+                                <label htmlFor="me">비공개</label>
+                            </div>
+
+                            <div className={styles.box2_2_2}>
+                                <img className={styles.myProfileImg} src={loginUser.profileUrl} />
+                                <div className={styles.textbox}>
+                                    <textarea placeholder="2024년 5월 30일, 오늘의 수행을 도반들에게 나눠주세요."></textarea>
+                                    <div className={styles.submit}>전송</div>
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <div
+                            className={styles.loginButton}
+                            onClick={() => {
+                                login('google');
+                            }}
+                        >
+                            구글 로그인
+                        </div>
+                    )}
                 </div>
-              </div>
-            </div>
-          ) : (
-            <div className={styles.loginButton}>로그인하기</div>
-          )}
-        </div>
 
-        <div className={styles.box1}>
-          <div className={styles.title}>
-            함께 <span className={styles.highlight}>수행</span>
-          </div>
-          <div>
-            혼자 하는 수행이 아닙니다
-            <br />
-            우리는 모두 <span className={styles.highlight}>연결</span>되어
-            있습니다
-          </div>
-          <div>
-            지금 이 순간에도, 보이지 않는 곳에서
-            <br />
-            도반님들이 함께 수행에 참여하고 있습니다.
-          </div>
-          {/* <img src={candlelight} /> */}
-          <div className={styles.box1_3}>
-            <div>
-              오늘 <span className={styles.cnt}>{cnt}</span> 명의 수행자들이
-              함께 하고 있습니다
-            </div>
-            <div className={styles.current_users_box}>
-              {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((x, i) => (
-                <div key={i} className={styles.current_user}>
-                  홍길동 도반님
+                <div className={styles.box1}>
+                    <div className={styles.title}>
+                        함께 <span className={styles.highlight}>수행</span>
+                    </div>
+                    <div>
+                        혼자 하는 수행이 아닙니다
+                        <br />
+                        우리는 모두 <span className={styles.highlight}>연결</span>되어 있습니다
+                    </div>
+                    <div>
+                        지금 이 순간에도, 보이지 않는 곳에서
+                        <br />
+                        도반님들이 함께 수행에 참여하고 있습니다.
+                    </div>
+                    {/* <img src={candlelight} /> */}
+                    <div className={styles.box1_3}>
+                        <div>
+                            오늘 <span className={styles.cnt}>{cnt}</span> 명의 수행자들이 함께 하고 있습니다
+                        </div>
+                        <div className={styles.current_users_box}>
+                            {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((x, i) => (
+                                <div key={i} className={styles.current_user}>
+                                    홍길동 도반님
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
-              ))}
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
