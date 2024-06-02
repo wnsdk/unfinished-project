c40c957695cd0c0c341a777450eab7035c1f8cef
wnsdk <jn307742@gmail.com>
2024-06-02 12:56:23 +0900

    Fix layout

--- frontend/suhaeng/src/pages/MainPage.jsx
+++ frontend/suhaeng/src/pages/MainPage.jsx
@@ -1,72 +1,96 @@
-import styles from './MainPage.module.scss';
-import Component1 from '@/components/Component1';
-import { useQuery } from '@tanstack/react-query';
-import { customAxios } from '../apis/customAxios';
+import styles from "./MainPage.module.scss";
+import Component1 from "@/components/Component1";
+import { useQuery } from "@tanstack/react-query";
+import { customAxios } from "../apis/customAxios";
 // import candlelight from '@/assets/다운로드-removebg-preview.png';
-import { useState } from 'react';
-import { useLoginStore } from '@/store/memberStore';
+import { useState } from "react";
+import { useLoginStore } from "@/store/memberStore";
 
 export default function MainPage() {
-    const {
-        isLoading,
-        error,
-        data: feeds,
-        isFetching,
-    } = useQuery({
-        queryKey: ['getFeeds'],
-        queryFn: async () => {
-            const response = await customAxios.get('feed');
-            console.log(response.data);
-            console.log(typeof response.data);
-            return response.data;
-        },
-    });
+  const {
+    isLoading,
+    error,
+    data: feeds,
+    isFetching,
+  } = useQuery({
+    queryKey: ["getFeeds"],
+    queryFn: async () => {
+      const response = await customAxios.get("feed");
+      return response.data;
+    },
+  });
 
-    const [cnt, setCnt] = useState(3124);
+  const [cnt, setCnt] = useState(3124);
 
-    const myProfileUrl = useLoginStore((state) => state.profileUrl);
+  const loginUser = useLoginStore((state) => state);
 
-    return (
-        <div className={styles.body}>
-            <div className={styles.gradient}>
-                <div className={styles.box2}>
-                    <div className={styles.box2_1}>
-                        {!isLoading && feeds.map((feed, i) => <Component1 key={i} feed={feed} />)}
-                    </div>
-                    <div className={styles.box2_2}>
-                        <div>
-                            <input type="radio" id="all" name="permission" />
-                            <label for="all">전체 공개</label>
-                            <input type="radio" id="group" name="permission" />
-                            <label for="group">그룹 공개</label>
-                            <input type="radio" id="me" name="permission" />
-                            <label for="me">비공개</label>
-                        </div>
-                        <div>
-                            <img className={styles.myProfileImg} src={myProfileUrl} />
-                            <div>
-                                <textarea placeholder="2024년 5월 30일, 오늘의 수행을 도반들에게 나눠주세요."></textarea>
-                                <div>전송</div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* <div className={styles.button}>로그인하기</div> */}
+  return (
+    <div className={styles.body}>
+      <div className={styles.gradient}>
+        <div className={styles.box2}>
+          <div className={styles.box2_1}>
+            {!isLoading &&
+              feeds.map((feed, i) => <Component1 key={i} feed={feed} />)}
+          </div>
+
+          {loginUser.isLogin ? (
+            <div className={styles.box2_2}>
+              <div>
+                <input type="radio" id="all" name="permission" />
+                <label htmlFor="all">전체 공개</label>
+                <input type="radio" id="group" name="permission" />
+                <label htmlFor="group">그룹 공개</label>
+                <input type="radio" id="me" name="permission" />
+                <label htmlFor="me">비공개</label>
+              </div>
+
+              <div className={styles.box2_2_2}>
+                <img
+                  className={styles.myProfileImg}
+                  src={loginUser.profileUrl}
+                />
+                <div className={styles.textbox}>
+                  <textarea placeholder="2024년 5월 30일, 오늘의 수행을 도반들에게 나눠주세요."></textarea>
+                  <div className={styles.submit}>전송</div>
                 </div>
-
-                <div className={styles.box1}>
-                    <div>
-                        <div>혼자 하는 수행이 아닙니다</div>
-                        <div>우리는 모두 연결되어 있습니다</div>
-                    </div>
-                    {/* <img src={candlelight} /> */}
-                    <div>
-                        <div>
-                            오늘 <span>{cnt}</span> 명의 수행자들이 함께 하고 있습니다
-                        </div>
-                        <div>ㅇㅇㅇ</div>
-                    </div>
-                </div>
+              </div>
             </div>
+          ) : (
+            <div className={styles.loginButton}>로그인하기</div>
+          )}
         </div>
-    );
+
+        <div className={styles.box1}>
+          <div className={styles.title}>
+            함께 <span className={styles.highlight}>수행</span>
+          </div>
+          <div>
+            혼자 하는 수행이 아닙니다
+            <br />
+            우리는 모두 <span className={styles.highlight}>연결</span>되어
+            있습니다
+          </div>
+          <div>
+            지금 이 순간에도, 보이지 않는 곳에서
+            <br />
+            도반님들이 함께 수행에 참여하고 있습니다.
+          </div>
+          {/* <img src={candlelight} /> */}
+          <div className={styles.box1_3}>
+            <div>
+              오늘 <span className={styles.cnt}>{cnt}</span> 명의 수행자들이
+              함께 하고 있습니다
+            </div>
+            <div className={styles.current_users_box}>
+              {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((x, i) => (
+                <div key={i} className={styles.current_user}>
+                  홍길동 도반님
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- frontend/suhaeng/src/store/memberStore.js
+++ frontend/suhaeng/src/store/memberStore.js
@@ -1,11 +1,12 @@
-import { create } from 'zustand';
+import { create } from "zustand";
 
 const useLoginStore = create((set) => ({
-    profileUrl: 'https://bit.ly/3V2xkYB',
-    memberName: '김실험',
-    email: 'zzz@gmail.com',
-    // plusCounter: () => set((state) => ({ counter: state.counter + 1 })),
-    // resetCounter: () => set({ counter: 0 }),
+  isLogin: false,
+  profileUrl: null,
+  memberName: null,
+  email: null,
+  // plusCounter: () => set((state) => ({ counter: state.counter + 1 })),
+  // resetCounter: () => set({ counter: 0 }),
 }));
 
 export { useLoginStore };
